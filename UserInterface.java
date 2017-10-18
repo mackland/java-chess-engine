de64a56906373caf3721612dd3bafbeae913465a
Marcus Ackland <mackland@kth.se>
2017-10-18 22:15:35 +0200

    class to handle graphics

---
+++ UserInterface.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class UserInterface extends JPanel{
+    public void paintComponent(Graphics g){
+        g.setColor(Color.BLUE);
+        g.fillRect(10, 40, 80, 200);
+        g.setColor(new Color(190, 81 215));
+        g.fillRect(10, 10, 20, 20,);
+    }
+}
